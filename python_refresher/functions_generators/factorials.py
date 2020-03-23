c2a5ef0f3b12127d9ccedf3b6fc995ff373d4270
huyenuet <thannhuyenk59uet@gmail.com>
2020-03-23 22:15:47 +0700

    functions and generators: exercise 1

---
+++ python_refresher/functions_generators/factorials.py
@@ -0,0 +1,27 @@
+def prod(a, b):
+    return a*b
+
+
+def fact_gen():
+    i = 1
+    n = i
+    while True:
+        output = prod(n, i)
+        yield output
+        # Hint: i is a successive integer and n is the previous product
+        i += 1
+        n = output
+
+
+# Test block
+my_gen = fact_gen()
+num = 5
+for i in range(num):
+    print(next(my_gen))
+
+# Correct result when num = 5:
+# 1
+# 2
+# 6
+# 24
+# 120
