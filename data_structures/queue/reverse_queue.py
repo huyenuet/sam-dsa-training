3e5a5858577a3c8d4d7812455b9eb9bffb0522fd
huyenuet <thannhuyenk59uet@gmail.com>
2020-04-05 18:00:20 +0700

    data structure: queue - reverse a queue using stack

---
+++ data_structures/queue/reverse_queue.py
@@ -0,0 +1,35 @@
+from data_structures.queue.queues_linked_list import Queue
+from data_structures.stack.stack_linkedlist import Stack
+
+
+def reverse_queue(queue):
+    stack = Stack()
+    while not queue.is_empty():
+        stack.push(queue.dequeue())
+
+    while not stack.is_empty():
+        queue.enqueue(stack.pop())
+
+
+def test_function(test_case):
+    queue = Queue()
+    for num in test_case:
+        queue.enqueue(num)
+
+    reverse_queue(queue)
+    index = len(test_case) - 1
+    while not queue.is_empty():
+        removed = queue.dequeue()
+        if removed != test_case[index]:
+            print("Fail")
+            return
+        else:
+            index -= 1
+    print("Pass")
+
+
+test_case_1 = [1, 2, 3, 4]
+test_function(test_case_1)
+
+test_case_2 = [1]
+test_function(test_case_2)
