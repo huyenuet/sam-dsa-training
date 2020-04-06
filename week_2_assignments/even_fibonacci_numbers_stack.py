ff4311e908134432726968b78dc0c287aed8f67c
huyenuet <thannhuyenk59uet@gmail.com>
2020-04-06 23:30:13 +0700

    week 2 assignments: calculate summary of even fibonacci numbers using Stack

---
+++ week_2_assignments/even_fibonacci_numbers_stack.py
@@ -0,0 +1,27 @@
+from data_structures.stack.stack_linkedlist import Stack
+
+threshold = 4000000000
+
+
+def even_fibonacci_numbers():
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+    even_sum = 0
+    while True:
+        second = stack.pop()
+        first = stack.pop()
+        if second > threshold:
+            break
+        if second % 2 == 0:
+            even_sum += second
+        third = first + second
+        stack.push(second)
+        stack.push(third)
+    return even_sum
+
+
+print(
+    "By considering the terms in the Fibonacci sequence whose values do not exceed four million, the sum of the even-valued terms is:"
+)
+print(even_fibonacci_numbers())
