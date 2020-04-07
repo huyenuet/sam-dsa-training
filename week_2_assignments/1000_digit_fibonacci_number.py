4392e5b182746f1d46e9690c669a4f915d6c9b72
huyenuet <thannhuyenk59uet@gmail.com>
2020-04-07 21:26:57 +0700

    week 2 assignments: find the index of the first term in the Fibonacci sequence to contain 1000 digits

---
+++ week_2_assignments/1000_digit_fibonacci_number.py
@@ -0,0 +1,19 @@
+"""
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+"""
+
+
+def find_1000_digit_fibonacci_numbers(number_digits):
+    fibo_1 = 1
+    fibo_2 = 1
+    index = 2
+    while True:
+        fibo_3 = fibo_1 + fibo_2
+        index += 1
+        if fibo_3 >= number_digits:
+            return index
+        fibo_1, fibo_2 = fibo_2, fibo_3
+
+
+print(find_1000_digit_fibonacci_numbers(pow(10, 2)))
+print(find_1000_digit_fibonacci_numbers(pow(10, 1000-1)))
