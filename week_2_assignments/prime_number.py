170f5005685d220557c98d53e7058ccf8c006dd7
huyenuet <thannhuyenk59uet@gmail.com>
2020-04-09 23:34:58 +0700

    fix: refactor for loop, conditional statement

--- week_2_assignments/prime_number.py
+++ week_2_assignments/prime_number.py
@@ -2,18 +2,18 @@
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 What is the 10 001st prime number?
 """
+from math import sqrt
 
 
 def is_prime_number(number):
     if number == 2:
         return True
-    elif number < 2:
+    if number < 2:
         return False
-    else:
-        for i in range(2, int(number / 2) + 1):
-            if number % i == 0:
-                return False
-        return True
+    for i in range(2, int(sqrt(number)) + 1):
+        if number % i == 0:
+            return False
+    return True
 
 
 def find_nth_prime_number(position):
