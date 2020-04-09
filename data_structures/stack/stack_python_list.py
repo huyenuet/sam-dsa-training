eef1bc08e8659614fc3e1e69fbad4afe3c0b2acd
huyenuet <thannhuyenk59uet@gmail.com>
2020-04-09 23:35:33 +0700

    data structure: stack - build a stack using a python list

---
+++ data_structures/stack/stack_python_list.py
@@ -0,0 +1,47 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def pop(self):
+        if self.is_empty():
+            return None
+
+        return self.stack.pop()
+
+    def top(self):
+        if self.is_empty():
+            return None
+        return self.stack[-1]
+
+
+def test():
+    # Setup
+    stack = Stack()
+    stack.push(10)
+    stack.push(20)
+    stack.push(30)
+    stack.push(40)
+    stack.push(50)
+
+    # Test size
+    print("Pass" if (stack.size() == 5) else "Fail")
+
+    # Test pop
+    print("Pass" if (stack.pop() == 50) else "Fail")
+
+    # Test push
+    stack.push(60)
+    print("Pass" if (stack.pop() == 60) else "Fail")
+    print("Pass" if (stack.pop() == 40) else "Fail")
+    print("Pass" if (stack.pop() == 30) else "Fail")
+    stack.push(50)
+    print("Pass" if (stack.size() == 3) else "Fail")
+
+
+test()
