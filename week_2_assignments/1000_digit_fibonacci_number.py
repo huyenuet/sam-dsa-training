aed05ea4ec61ec75fff49596093a90b76bd5af44
huyenuet <thannhuyenk59uet@gmail.com>
2020-04-09 23:29:42 +0700

    fix: rename parameter

--- week_2_assignments/1000_digit_fibonacci_number.py
+++ week_2_assignments/1000_digit_fibonacci_number.py
@@ -3,14 +3,14 @@ What is the index of the first term in the Fibonacci sequence to contain 1000 di
 """
 
 
-def find_1000_digit_fibonacci_numbers(number_digits):
+def find_1000_digit_fibonacci_numbers(threshold):
     fibo_1 = 1
     fibo_2 = 1
     index = 2
     while True:
         fibo_3 = fibo_1 + fibo_2
         index += 1
-        if fibo_3 >= number_digits:
+        if fibo_3 >= threshold:
             return index
         fibo_1, fibo_2 = fibo_2, fibo_3
 
