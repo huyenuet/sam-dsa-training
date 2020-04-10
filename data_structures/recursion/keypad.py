5e6b1701068eb5caeb4aa20eb05b3ebdda0952c0
huyenuet <thannhuyenk59uet@gmail.com>
2020-04-10 23:43:23 +0700

    recursion: Keypad Combinations

---
+++ data_structures/recursion/keypad.py
@@ -0,0 +1,65 @@
+def keypad(num):
+    if num <= 1:
+        return [""]
+    if 1 < num <= 9:
+        return list(get_characters(num))
+
+    last_digit = num % 10
+    sub_num = num // 10
+    sub_keypad = keypad(sub_num)
+    keypad_string = get_characters(last_digit)
+    output = list()
+    for character in keypad_string:
+        for item in sub_keypad:
+            new_item = item + character
+            output.append(new_item)
+    return output
+
+
+def get_characters(num):
+    if num == 2:
+        return "abc"
+    elif num == 3:
+        return "def"
+    elif num == 4:
+        return "ghi"
+    elif num == 5:
+        return "jkl"
+    elif num == 6:
+        return "mno"
+    elif num == 7:
+        return "pqrs"
+    elif num == 8:
+        return "tuv"
+    elif num == 9:
+        return "wxyz"
+    else:
+        return ""
+
+
+def test_keypad(input, expected_output):
+    if sorted(keypad(input)) == expected_output:
+        print("Yay. We got it right.")
+    else:
+        print("Oops! That was incorrect.")
+
+
+input = 0
+expected_output = [""]
+test_keypad(input, expected_output)
+
+input = 23
+expected_output = sorted(["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"])
+test_keypad(input, expected_output)
+
+input = 32
+expected_output = sorted(["da", "db", "dc", "ea", "eb", "ec", "fa", "fb", "fc"])
+test_keypad(input, expected_output)
+
+input = 8
+expected_output = sorted(["t", "u", "v"])
+test_keypad(input, expected_output)
+
+input = 354
+expected_output = sorted(["djg", "ejg", "fjg", "dkg", "ekg", "fkg", "dlg", "elg", "flg", "djh", "ejh", "fjh", "dkh", "ekh", "fkh", "dlh", "elh", "flh", "dji", "eji", "fji", "dki", "eki", "fki", "dli", "eli", "fli"])
+test_keypad(input, expected_output)
