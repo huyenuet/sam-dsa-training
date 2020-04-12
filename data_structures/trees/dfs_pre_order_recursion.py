e29245bca1d072ec1d085873c76e8beb9d319386
huyenuet <thannhuyenk59uet@gmail.com>
2020-04-13 00:12:19 +0700

    tree: pre-order depth first search using recursion

---
+++ data_structures/trees/dfs_pre_order_recursion.py
@@ -0,0 +1,33 @@
+from data_structures.trees.tree import Tree
+from data_structures.trees.tree_node import Node
+
+
+# init tree
+tree = Tree("apple")
+tree.get_root().set_left_child(Node("banana"))
+tree.get_root().set_right_child(Node("cherry"))
+tree.get_root().get_left_child().set_left_child(Node("dates"))
+
+
+def dfs_pre_order_recursion(tree: Tree):
+    visit_list = list()
+    node = tree.get_root()
+
+    # traverse
+    def traverse(node):
+        if node:
+            # visit the node
+            visit_list.append(node.get_value())
+
+            # traverse left
+            traverse(node.get_left_child())
+
+            # traverse right
+            traverse(node.get_right_child())
+
+    traverse(node)
+
+    return visit_list
+
+
+print(dfs_pre_order_recursion(tree))
