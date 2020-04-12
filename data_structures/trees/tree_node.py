3e221f3b040ce00fe877acf629fcda8de813eb0d
huyenuet <thannhuyenk59uet@gmail.com>
2020-04-13 00:11:30 +0700

    tree: init a class for Tree Node

---
+++ data_structures/trees/tree_node.py
@@ -0,0 +1,35 @@
+class Node(object):
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def get_left_child(self):
+        return self.left
+
+    def get_right_child(self):
+        return self.right
+
+    def set_left_child(self, node):
+        self.left = node
+
+    def set_right_child(self, node):
+        self.right = node
+
+    def has_left_child(self):
+        return self.left is not None
+
+    def has_right_child(self):
+        return self.right is not None
+
+    def __str__(self):
+        return f"Node({self.get_value()})"
+
+    def __repr__(self):
+        return f"Node({self.get_value()})"
