de264d31a36c16a97372b8c3c3311338f4b0fe7c
huyenuet <thannhuyenk59uet@gmail.com>
2020-04-14 00:16:42 +0700

    tree: test search function

--- data_structures/trees/insert_in_a_tree.py
+++ data_structures/trees/insert_in_a_tree.py
@@ -11,6 +11,7 @@ tree.insert_with_loop(4)
 tree.insert_with_loop(0)
 print(tree)
 
+print("\n=====================")
 tree = Tree(1)
 tree.insert_with_recursion(2)
 tree.insert_with_recursion(3)
@@ -18,3 +19,8 @@ tree.insert_with_recursion(5)
 tree.insert_with_recursion(4)
 tree.insert_with_recursion(0)
 print(tree)
+
+
+print("\n=====================")
+print(tree.search(0).get_value())
+print(tree.search(10))
