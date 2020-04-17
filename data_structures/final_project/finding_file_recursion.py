753c7440132792b3d4e03916ce83eac2669720f5
huyenuet <thannhuyenk59uet@gmail.com>
2020-04-17 22:03:39 +0700

    final project: finding files under a directory (and all directories beneath it) that end with ".c"

---
+++ data_structures/final_project/finding_file_recursion.py
@@ -0,0 +1,45 @@
+import os
+
+
+def find_files(suffix, path):
+    """
+    Find all files beneath path with file name suffix.
+
+    Note that a path may contain further subdirectories
+    and those subdirectories may also contain further subdirectories.
+
+    There are no limit to the depth of the subdirectories can be.
+
+    Args:
+      suffix(str): suffix if the file name to be found
+      path(str): path of the file system
+
+    Returns:
+       a list of paths
+    """
+    path_ls = list()
+    sub_dirs = os.listdir(path=path)
+    for sub_dir in sub_dirs:
+        sub_path = os.path.join(path, sub_dir)
+        if os.path.isfile(sub_path) and sub_dir.endswith(suffix):
+            path_ls.append(sub_path)
+        if os.path.isdir(sub_path):
+            output = find_files(suffix, sub_path)
+            path_ls += output
+
+    return path_ls
+
+
+# some useful os methods
+print(os.path.isdir('testdir'))
+print(os.path.isfile('finding_file_recursion.py'))
+print(os.listdir('.'))
+print(os.listdir('./testdir'))
+print(os.path.join('testdir', 'test'))
+print("./ex.py".endswith(".py"))
+
+
+# test the method find_files
+test_path = "./testdir"
+test_suffix = ".c"
+print(find_files(test_suffix, test_path))
