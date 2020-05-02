6182783ba1071952105966c01f172bfbf21e55d9
huyenuet <thannhuyenk59uet@gmail.com>
2020-05-02 14:38:06 +0700

    chore: refactor code

--- basic_algorithms/sort/bubble_sort_exercises_1.py
+++ basic_algorithms/sort/bubble_sort_exercises_1.py
@@ -31,11 +31,12 @@ def bubble_sort_2(ls):
 
 
 wakeup_times = [16, 49, 3, 12, 56, 49, 55, 22, 13, 46, 19, 55, 46, 13, 25, 56, 9, 48, 45]
+expected_result = [3, 9, 12, 13, 13, 16, 19, 22, 25, 45, 46, 46, 48, 49, 49, 55, 55, 56, 56]
 
 
 def test(func):
     func(wakeup_times)
-    print("Pass" if (wakeup_times == [3, 9, 12, 13, 13, 16, 19, 22, 25, 45, 46, 46, 48, 49, 49, 55, 55, 56, 56]) else "Fail")
+    print("Pass" if (wakeup_times == expected_result) else "Fail")
 
 
 test(bubble_sort_1)
--- basic_algorithms/sort/bubble_sort_exercises_2.py
+++ basic_algorithms/sort/bubble_sort_exercises_2.py
@@ -29,11 +29,12 @@ def bubble_sort_2(l):
 
 # Entries are (h, m) where h is the hour and m is the minute
 sleep_times = [(24, 13), (21, 55), (23, 20), (22, 5), (24, 23), (21, 58), (24, 3)]
+expected_result = [(24, 23), (24, 13), (24, 3), (23, 20), (22, 5), (21, 58), (21, 55)]
 
 
 def test(func):
     func(sleep_times)
-    print("Pass" if (sleep_times == [(24, 23), (24, 13), (24, 3), (23, 20), (22, 5), (21, 58), (21, 55)]) else "Fail")
+    print("Pass" if (sleep_times == expected_result) else "Fail")
 
 
 test(bubble_sort_1)
