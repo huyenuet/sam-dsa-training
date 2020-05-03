c88fcf5acff7b985b67c480a0f44e994997467e8
huyenuet <thannhuyenk59uet@gmail.com>
2020-05-03 11:34:12 +0700

    basic algorithm: merged sort

---
+++ basic_algorithms/sort/merged_sort.py
@@ -0,0 +1,39 @@
+def merged_sort(items):
+    if len(items) <= 1:
+        return items
+
+    mid = len(items) // 2
+    left = items[:mid]
+    right = items[mid:]
+
+    left = merged_sort(left)
+    right = merged_sort(right)
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] > right[right_index]:
+            merged.append(right[right_index])
+            right_index += 1
+        else:
+            merged.append(left[left_index])
+            left_index += 1
+
+    merged += left[left_index:]
+    merged += right[right_index:]
+
+    return merged
+
+
+test_list_1 = [8, 3, 1, 7, 0, 10, 2]
+test_list_2 = [1, 0]
+test_list_3 = [97, 98, 99]
+print('{} to {}'.format(test_list_1, merged_sort(test_list_1)))
+print('{} to {}'.format(test_list_2, merged_sort(test_list_2)))
+print('{} to {}'.format(test_list_3, merged_sort(test_list_3)))
