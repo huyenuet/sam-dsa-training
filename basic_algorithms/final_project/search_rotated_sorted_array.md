875debaa2561813856b704e1bdc7f52536330949
huyenuet <thannhuyenk59uet@gmail.com>
2020-05-07 22:05:18 +0700

    Basic algorithm - final project - problem 2: search in a rotated sorted array

---
+++ basic_algorithms/final_project/search_rotated_sorted_array.md
@@ -0,0 +1,15 @@
+### Approach
+If the pivot point is found, I can decide to find 
+the target number in which sub-array
+- from 0th  to pivot_index element
+- or from pivot_index + 1 to the last element
+
+From there, I would continue to use binary search to
+traverse through sub-array, and find the target value
+
+
+### Efficiency
+- Time complexity:
+    - find pivot index: O(m)
+    - find target value's index: O(logn)
+- Space complexity: O(1)
---
+++ basic_algorithms/final_project/search_rotated_sorted_array.py
@@ -0,0 +1,49 @@
+def rotated_array_search(input_list, target):
+    # find pivot point
+    pivot_index = 0
+    for i in range(len(input_list) - 1):
+        if input_list[i] > input_list[i + 1]:
+            pivot_index = i
+            break
+
+    if target == input_list[0]:
+        return 0
+    elif target > input_list[0]:
+        return binary_search(input_list, target, 0, pivot_index)
+    else:
+        return binary_search(input_list, target, pivot_index + 1, len(input_list))
+
+
+def binary_search(input_list, target, left, right):
+    mid = (left + right) // 2
+    if left > right:
+        return -1
+    if input_list[mid] == target:
+        return mid
+    elif input_list[mid] > target:
+        return binary_search(input_list, target, left, mid - 1)
+    else:
+        return binary_search(input_list, target, mid + 1, right)
+
+
+def linear_search(input_list, number):
+    for index, element in enumerate(input_list):
+        if element == number:
+            return index
+    return -1
+
+
+def test_function(test_case):
+    input_list = test_case[0]
+    number = test_case[1]
+    if linear_search(input_list, number) == rotated_array_search(input_list, number):
+        print("Pass")
+    else:
+        print("Fail")
+
+
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
+test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
+test_function([[6, 7, 8, 1, 2, 3, 4], 8])
+test_function([[6, 7, 8, 1, 2, 3, 4], 1])
+test_function([[6, 7, 8, 1, 2, 3, 4], 10])
