e043735f0cc82f451067a618a881903df4dae4f0
huyenuet <thannhuyenk59uet@gmail.com>
2020-05-21 23:20:25 +0700

    game solver: optimize code a little bit

--- assignment/game_solver.py
+++ assignment/game_solver.py
@@ -23,17 +23,89 @@ def solve_game(input_arr):
         raise Exception("Array size must be greater than or equal to 4")
 
     temp_arr = apply_rule_1(input_arr)
-    print_two_dimensional_arr(temp_arr)
 
     while not is_game_solved(input_arr):
         temp_arr = apply_rule_2(temp_arr)
-        print_two_dimensional_arr(temp_arr)
         temp_arr = apply_rule_3(temp_arr)
-        print_two_dimensional_arr(temp_arr)
         temp_arr = apply_rule_1(temp_arr)
-        print_two_dimensional_arr(temp_arr)
 
     return temp_arr
+
+
+def apply_rule_1(input_arr):
+    """
+    No three consecutive tiles have the same colour.
+    """
+    arr_size = len(input_arr)
+
+    # check row
+    for row in input_arr:
+        indices_of_2 = []
+        indices_of_1 = []
+        for index, num in enumerate(row):
+            if num == 1:
+                indices_of_1.append(index)
+            elif num == 2:
+                indices_of_2.append(index)
+        i = 0
+        while i < len(indices_of_2) and i + 1 < len(indices_of_2):
+
+            # 1. if there are 2 consecutive tiles have the same colour,
+            # the next and previous tiles should be different colors
+            if indices_of_2[i + 1] == indices_of_2[i] + 2:
+                row[indices_of_2[i] + 1] = 1
+
+            # 2. if there are 2 tiles distributed like 2, 0, 2 => 0 should be replaced by 1
+            elif indices_of_2[i + 1] == indices_of_2[i] + 1:
+                if indices_of_2[i] + 2 < arr_size:
+                    row[indices_of_2[i] + 2] = 1
+                if indices_of_2[i] >= 1:
+                    row[indices_of_2[i] - 1] = 1
+            i += 1
+
+        i = 0
+        while i < len(indices_of_1) and i + 1 < len(indices_of_1):
+            if indices_of_1[i + 1] == indices_of_1[i] + 2:
+                row[indices_of_1[i] + 1] = 2
+            elif indices_of_1[i + 1] == indices_of_1[i] + 1:
+                if indices_of_1[i] + 2 < arr_size:
+                    row[indices_of_1[i] + 2] = 2
+                if indices_of_1[i] >= 1:
+                    row[indices_of_1[i] - 1] = 2
+            i += 1
+
+    # check column
+    for col_idx in range(0, arr_size):
+        indices_of_2 = []
+        indices_of_1 = []
+        for row_idx in range(0, arr_size):
+            if input_arr[row_idx][col_idx] == 1:
+                indices_of_1.append(row_idx)
+            if input_arr[row_idx][col_idx] == 2:
+                indices_of_2.append(row_idx)
+        i = 0
+        while i < len(indices_of_2) and i + 1 < len(indices_of_2):
+            if indices_of_2[i + 1] == indices_of_2[i] + 2:
+                input_arr[indices_of_2[i] + 1][col_idx] = 1
+            elif indices_of_2[i + 1] == indices_of_2[i] + 1:
+                if indices_of_2[i] + 2 < arr_size:
+                    input_arr[indices_of_2[i] + 2][col_idx] = 1
+                if indices_of_2[i] >= 1:
+                    input_arr[indices_of_2[i] - 1][col_idx] = 1
+            i += 1
+
+        i = 0
+        while i < len(indices_of_1) and i + 1 < len(indices_of_1):
+            if indices_of_1[i + 1] == indices_of_1[i] + 2:
+                input_arr[indices_of_1[i] + 1][col_idx] = 2
+            elif indices_of_1[i + 1] == indices_of_1[i] + 1:
+                if indices_of_1[i] + 2 < arr_size:
+                    input_arr[indices_of_1[i] + 2][col_idx] = 2
+                if indices_of_1[i] >= 1:
+                    input_arr[indices_of_1[i] - 1][col_idx] = 2
+            i += 1
+
+    return input_arr
 
 
 def apply_rule_2(input_arr):
@@ -43,22 +115,13 @@ def apply_rule_2(input_arr):
 
     # check row
     for row_idx, row in enumerate(input_arr):
-        total_1 = 0
-        total_2 = 0
+        total_1 = row.count(1)
+        total_2 = row.count(2)
 
-        for num in row:
-            if num == 1:
-                total_1 += 1
-            elif num == 2:
-                total_2 += 1
         if total_1 == (len(input_arr) / 2):
-            for index, num in enumerate(row):
-                if num == 0:
-                    input_arr[row_idx][index] = 2
+            input_arr[row_idx] = [2 if num == 0 else num for num in row]
         elif total_2 == (len(input_arr) / 2):
-            for index, num in enumerate(row):
-                if num == 0:
-                    input_arr[row_idx][index] = 1
+            input_arr[row_idx] = [1 if num == 0 else num for num in row]
 
     # check column
     arr_size = len(input_arr)
@@ -78,110 +141,6 @@ def apply_rule_2(input_arr):
             for j in range(0, arr_size):
                 if input_arr[j][i] == 0:
                     input_arr[j][i] = 1
-
-    return input_arr
-
-
-def apply_rule_1(input_arr):
-    """
-    No three consecutive tiles have the same colour.
-    """
-    arr_size = len(input_arr)
-
-    # 1. if there are 2 consecutive tiles have the same colour, the next and previous tiles should be different color
-    # check row
-    for row_idx, row in enumerate(input_arr):
-        consecutive_1 = 0
-        consecutive_2 = 0
-
-        for col_idx, num in enumerate(row):
-            if num == 1:
-                consecutive_1 += 1
-                consecutive_2 = 0
-                if consecutive_1 == 2:
-                    if col_idx + 1 < arr_size:
-                        input_arr[row_idx][col_idx + 1] = 2
-                    if col_idx >= 2:
-                        input_arr[row_idx][col_idx - 2] = 2
-            elif num == 2:
-                consecutive_2 += 1
-                consecutive_1 = 0
-                if consecutive_2 == 2:
-                    if col_idx + 1 < arr_size:
-                        input_arr[row_idx][col_idx + 1] = 1
-                    if col_idx >= 2:
-                        input_arr[row_idx][col_idx - 2] = 1
-            else:
-                consecutive_1 = 0
-                consecutive_2 = 0
-
-    # check column
-    for i in range(0, arr_size):
-        consecutive_1 = 0
-        consecutive_2 = 0
-        for j in range(0, arr_size):
-            if input_arr[j][i] == 1:
-                consecutive_1 += 1
-                consecutive_2 = 0
-                if consecutive_1 == 2:
-                    if j + 1 < arr_size:
-                        input_arr[j + 1][i] = 2
-                    if j >= 2:
-                        input_arr[j - 2][i] = 2
-            elif input_arr[j][i] == 2:
-                consecutive_2 += 1
-                consecutive_1 = 0
-                if consecutive_2 == 2:
-                    if j + 1 < arr_size:
-                        input_arr[j + 1][i] = 1
-                    if j >= 2:
-                        input_arr[j - 2][i] = 1
-            else:
-                consecutive_2 = 0
-                consecutive_1 = 0
-
-    # 2. if there 2 same tiles distributed like 2, 0, 2 => 0 should be replaced by 1
-    # check row
-    for row in input_arr:
-        indices_of_2 = []
-        indices_of_1 = []
-        for index, num in enumerate(row):
-            if num == 1:
-                indices_of_1.append(index)
-            elif num == 2:
-                indices_of_2.append(index)
-        i = 0
-        while i < len(indices_of_2) and i + 1 < len(indices_of_2):
-            if indices_of_2[i + 1] == indices_of_2[i] + 2:
-                row[indices_of_2[i] + 1] = 1
-            i += 1
-
-        i = 0
-        while i < len(indices_of_1) and i + 1 < len(indices_of_1):
-            if indices_of_1[i + 1] == indices_of_1[i] + 2:
-                row[indices_of_1[i] + 1] = 2
-            i += 1
-
-    # check column
-    for col_idx in range(0, arr_size):
-        indices_of_2 = []
-        indices_of_1 = []
-        for row_idx in range(0, arr_size):
-            if input_arr[row_idx][col_idx] == 1:
-                indices_of_1.append(row_idx)
-            if input_arr[row_idx][col_idx] == 2:
-                indices_of_2.append(row_idx)
-        i = 0
-        while i < len(indices_of_2) and i + 1 < len(indices_of_2):
-            if indices_of_2[i + 1] == indices_of_2[i] + 2:
-                input_arr[indices_of_2[i] + 1][col_idx] = 1
-            i += 1
-
-        i = 0
-        while i < len(indices_of_1) and i + 1 < len(indices_of_1):
-            if indices_of_1[i + 1] == indices_of_1[i] + 2:
-                input_arr[indices_of_1[i] + 1][col_idx] = 2
-            i += 1
 
     return input_arr
 
@@ -265,3 +224,18 @@ def is_game_solved(input_arr):
 
 print_two_dimensional_arr(solve_game(example_1))
 print_two_dimensional_arr(solve_game(example_2))
+
+ex_1_result = [[1, 1, 2, 2],
+               [2, 1, 2, 1],
+               [1, 2, 1, 2],
+               [2, 2, 1, 1]]
+
+ex_2_result = [[1, 1, 2, 1, 1, 2],
+               [1, 1, 2, 1, 2, 1],
+               [2, 2, 1, 2, 1, 1],
+               [1, 2, 1, 2, 1, 2],
+               [1, 1, 2, 1, 2, 2],
+               [2, 2, 1, 2, 2, 1]]
+
+print("pass" if solve_game(example_1) == ex_1_result else "fail")
+print("pass" if solve_game(example_2) == ex_2_result else "fail")
