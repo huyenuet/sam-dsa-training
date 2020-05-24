024874b9f0288ce4a15e8c8f2c44662ea174d1e9
huyenuet <thannhuyenk59uet@gmail.com>
2020-05-24 23:18:06 +0700

    refactor(game solver): rename method, fix/add comments

--- assignment/game_solver.py
+++ assignment/game_solver.py
@@ -50,12 +50,12 @@ def apply_rule_1(input_arr):
         i = 0
         while i < len(indices_of_2) and i + 1 < len(indices_of_2):
 
-            # 1. if there are 2 consecutive tiles have the same color,
-            # the next and previous tiles should be a the other color
+            # 1. if there are 2 tiles distributed like 2, 0, 2 => 0 should be replaced by 1
             if indices_of_2[i + 1] == indices_of_2[i] + 2:
                 row[indices_of_2[i] + 1] = 1
 
-            # 2. if there are 2 tiles distributed like 2, 0, 2 => 0 should be replaced by 1
+            # 2. if there are 2 consecutive tiles have the same color(2),
+            # the next and previous tiles should be the other color(1)
             elif indices_of_2[i + 1] == indices_of_2[i] + 1:
                 if indices_of_2[i] + 2 < arr_size:
                     row[indices_of_2[i] + 2] = 1
@@ -112,32 +112,32 @@ def apply_rule_2(input_arr):
     """
     On each row or column of the grid, the number of red tiles equals to the number of blue ones.
     """
+    arr_size = len(input_arr)
 
     # check row
     for row_idx, row in enumerate(input_arr):
-        total_1 = row.count(1)
-        total_2 = row.count(2)
+        total_1_tiles = row.count(1)
+        total_2_tiles = row.count(2)
 
-        if total_1 == (len(input_arr) / 2):
+        if total_1_tiles == (arr_size / 2):
             input_arr[row_idx] = [2 if num == 0 else num for num in row]
-        elif total_2 == (len(input_arr) / 2):
+        elif total_2_tiles == (arr_size / 2):
             input_arr[row_idx] = [1 if num == 0 else num for num in row]
 
     # check column
-    arr_size = len(input_arr)
     for i in range(0, arr_size):
-        total_1 = 0
-        total_2 = 0
+        total_1_tiles = 0
+        total_2_tiles = 0
         for j in range(0, arr_size):
             if input_arr[j][i] == 1:
-                total_1 += 1
+                total_1_tiles += 1
             elif input_arr[j][i] == 2:
-                total_2 += 1
-        if total_1 == arr_size / 2:
+                total_2_tiles += 1
+        if total_1_tiles == arr_size / 2:
             for j in range(0, arr_size):
                 if input_arr[j][i] == 0:
                     input_arr[j][i] = 2
-        elif total_2 == arr_size / 2:
+        elif total_2_tiles == arr_size / 2:
             for j in range(0, arr_size):
                 if input_arr[j][i] == 0:
                     input_arr[j][i] = 1
@@ -154,20 +154,20 @@ def apply_rule_3(input_arr):
     # check column
     for col_idx in range(0, arr_size - 1):
         for next_col_idx in range(col_idx + 1, arr_size):
-            different_positions = []
+            different_tile_indices = []
             for row_idx in range(0, arr_size):
                 if input_arr[row_idx][col_idx] == input_arr[row_idx][next_col_idx] == 0:
-                    different_positions = []
+                    different_tile_indices = []
                     break
                 if (
                     (input_arr[row_idx][col_idx] == 0 or input_arr[row_idx][next_col_idx] == 0)
                     and (input_arr[row_idx][col_idx] != input_arr[row_idx][next_col_idx])
                 ):
-                    different_positions.append((row_idx, col_idx, next_col_idx))
+                    different_tile_indices.append((row_idx, col_idx, next_col_idx))
 
             # 2 columns are the same except 2 cells
-            if len(different_positions) == 2:
-                for row_idx, i, j in different_positions:
+            if len(different_tile_indices) == 2:
+                for row_idx, i, j in different_tile_indices:
                     if input_arr[row_idx][i] != 0 and input_arr[row_idx][j] == 0:
                         tiles = [1, 2]
                         tiles.pop(tiles.index(input_arr[row_idx][i]))
@@ -180,20 +180,20 @@ def apply_rule_3(input_arr):
     # check row
     for row_idx in range(0, arr_size - 1):
         for next_row_idx in range(row_idx + 1, arr_size):
-            different_positions = []
+            different_tile_indices = []
             for col_idx in range(0, arr_size):
                 if input_arr[row_idx][col_idx] == input_arr[next_row_idx][col_idx] == 0:
-                    different_positions = []
+                    different_tile_indices = []
                     break
                 if (
                     (input_arr[row_idx][col_idx] == 0 or input_arr[next_row_idx][col_idx] == 0)
                     and (input_arr[row_idx][col_idx] != input_arr[next_row_idx][col_idx])
                 ):
-                    different_positions.append((col_idx, row_idx, next_row_idx))
+                    different_tile_indices.append((col_idx, row_idx, next_row_idx))
 
             # 2 rows are the same except 2 cells
-            if len(different_positions) == 2:
-                for col_idx, i, j in different_positions:
+            if len(different_tile_indices) == 2:
+                for col_idx, i, j in different_tile_indices:
                     if input_arr[i][col_idx] != 0 and input_arr[j][col_idx] == 0:
                         tiles = [1, 2]
                         tiles.pop(tiles.index(input_arr[i][col_idx]))
@@ -206,6 +206,7 @@ def apply_rule_3(input_arr):
     return input_arr
 
 
+# this method is used for debugging
 def print_two_dimensional_arr(arr):
     for row in arr:
         print(row)
@@ -219,9 +220,6 @@ def is_game_solved(input_arr):
                 return False
     return True
 
-
-print_two_dimensional_arr(solve_game(example_1))
-print_two_dimensional_arr(solve_game(example_2))
 
 ex_1_result = [[1, 1, 2, 2],
                [2, 1, 2, 1],
